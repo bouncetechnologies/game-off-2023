d52b50ae1eb7fd8a634bd3d7083329b6f6d8a242
Alex Nicol <nicol.alexandre1@gmail.com>
2023-11-09 15:37:22 +0000

    level scaling 3

--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+#signal movement(velocity_x)
 
 const SPEED = 300.0
 
@@ -19,9 +20,19 @@ const JUMP_STEP = 10
 
 
 var jump_velocity = JUMP_MIN
+var velocity_x = 0
+var started = false
 
+var base_x_position = null
+
+func _ready():
+	base_x_position = self.position.x
 
 func _physics_process(delta):
+	print(base_x_position, self.position.x)
+	
+
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -32,21 +43,41 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-		_animated_sprite.flip_h = velocity.x < 0
+#	var direction = Input.get_axis("ui_left", "ui_right")
+#	if direction:
+##		velocity.x = direction * SPEED
+#		velocity_x = direction * SPEED
+#	else:
+##		velocity.x = move_toward(velocity.x, 0, SPEED)
+#		velocity_x = move_toward(velocity.x, 0, SPEED)
+#
+#	movement.emit(velocity_x)
+	if base_x_position > self.position.x:
+		velocity.x = SPEED / 2
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = 0
 
 	move_and_slide()
 
 
 func _process(delta):
-	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left"):
+#	if Input.is_action_pressed("ui_right"):
+#		_animated_sprite.flip_h = false
+#		_animated_sprite.play("default")
+#	elif Input.is_action_pressed("ui_left"):
+#		_animated_sprite.flip_h = true
+#		_animated_sprite.play("default")
+#	else :
+#		_animated_sprite.stop()
+		
+	if Input.is_action_pressed("ui_text_submit"):
+		start()
+		
+	if started:
 		_animated_sprite.play("default")
-	else :
-		_animated_sprite.stop()
+
+func start():
+	started = true
 		
 #	if Input.is_action_pressed("ui_up"):
 #		if self.scale.x < SCALE_MAX:
