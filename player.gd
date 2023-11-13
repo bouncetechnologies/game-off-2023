4f7f36222b030c2b3810c04619edc3e15a28d121
Alastair Byrne <alastairbyrne@gmail.com>
2023-11-13 16:01:50 +0000

    Update keybinds

--- player.gd
+++ player.gd
@@ -42,7 +42,7 @@ func _physics_process(delta):
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	
 	# Handle facing sprite
 	if direction == -1:
@@ -57,7 +57,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	# Handle jump ascend
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY * scale.y
 		animated_sprite.play("jump_ascending")
 		is_jumping = true
