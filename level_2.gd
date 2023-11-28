2c67225cc8102fcb4ffd83f8fbb07ab5a5b76b09
E <ethan@bouncebot.io>
2023-11-28 15:01:39 +0000

    Revert "Split out parallax background into new scene"
    This reverts commit 3356932e37bc968c86eb53ee031535d7e57a558f.

--- level_2.gd
+++ level_2.gd
@@ -8,4 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Parallax/ParallaxBackground.scale = $Player.scale
+	$ParallaxBackground.scale = $Player.scale
