3356932e37bc968c86eb53ee031535d7e57a558f
E <ethan@bouncebot.io>
2023-11-28 14:46:20 +0000

    Split out parallax background into new scene

--- level_2.gd
+++ level_2.gd
@@ -8,4 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$ParallaxBackground.scale = $Player.scale
+	$Parallax/ParallaxBackground.scale = $Player.scale
