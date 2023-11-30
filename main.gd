c0e783dd8031a21e39335014d9d3de0ae786a6cb
E <ethan@bouncebot.io>
2023-11-30 16:24:59 +0000

    Add end scene

--- main.gd
+++ main.gd
@@ -4,10 +4,9 @@ var wall_should_move = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$ParallaxBackground.scale = $Player.scale
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$ParallaxBackground.scale = $Player.scale
@@ -23,7 +22,6 @@ func _physics_process(delta):
 		$MovingWall.position.x = $Player.respawn.x - 200
 		$Timer.start()
 		
-
 
 func _on_timer_timeout():
 	wall_should_move = false
